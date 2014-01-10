d10527234449803e9aa8403947dc4547b7c19d56
Denis Popov <akadan47@gmail.com>
2014-01-10 19:15:04 +0400

    Fix for use new expression compiler

--- gekkon/tags/include.php
+++ gekkon/tags/include.php
@@ -2,7 +2,7 @@
 
 function gekkon_tag_include_single($compiler, $_tag)
 {
-    $exp = $compiler->compile_exp($_tag['raw_args']);
+    $exp = $compiler->exp_compiler->compile_exp($_tag['raw_args']);
     if($exp === false)
             return r_error('gekkon: tag include: Cannot compile expression');
     return '$gekkon->display('.$exp.");\n";
