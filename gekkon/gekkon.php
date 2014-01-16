0d1ff47a1c1ba20472006a89a0b995985a721c97
toecto <ad@zalil.ru>
2014-01-16 05:18:38 -0500

    minor fix

--- gekkon/gekkon.php
+++ gekkon/gekkon.php
@@ -57,7 +57,7 @@ class Gekkon {
         {
             include_once $bin_file;
         }
-        call_user_func($fn_nme, $this);
+        $fn_nme($this);
         $this->tpl_name = $tpl_name_save;
     }
 
