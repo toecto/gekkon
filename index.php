bbaee9faabb5cf912dc1821686dba8c9710369fd
toecto <ad@zalil.ru>
2014-01-22 03:00:13 -0500

    new compiler system

--- index.php
+++ index.php
@@ -2,11 +2,10 @@
 
 ini_set("display_errors", 'on');
 include "gekkon/gekkon.php";
-include_once "gekkon/ll_parser.php";
 
 
 
 $gekkon = new Gekkon(dirname(__file__).'/', dirname(__file__).'/tpl_bin/',
-        'tpl/');
+    'tpl/');
 $gekkon->display("test.tpl");
 
